a8f918f793d5157a6bd898fa2fa36db62ae4c9db
xanxys <xanxys@gmail.com>
2014-01-16 14:03:36 +0900

    Rank-1 Markov-chain learner and generator.

--- Main.hs
+++ Main.hs
@@ -2,6 +2,9 @@ import Control.Monad
 import Control.Monad.Random
 import System.Random
 
+import qualified Data.Map as M
+import qualified Data.Set as S
+
 
 voc = ["import", "class", "def", "\n", " ", "\t",
 	"Manager", "Web", "Server", "Client", "View", "Model",
@@ -24,5 +27,79 @@ randomCode = do
 --		(ix, gen'') = randomR (0, length voc - 1) gen'
 --		w = voc !! ix
 
+
+learn code = return ()
+
+	
+
+splitter :: String -> String -> [String]
+splitter buf "" = wrapBuffer buf
+splitter buf (x:xs)
+	|x `elem` delims = wrapBuffer buf ++ ([x] : splitter "" xs)
+	|otherwise = splitter (buf ++ [x]) xs
+	where
+		delims = symbols ++ spaces
+		symbols = "!\"#$%&'=~|-^\\,./?()[]<>{}"
+		spaces = " \t\n"
+
+wrapBuffer "" = []
+wrapBuffer buf = [buf]
+
+
+count2Gram :: Ord a => [a] -> M.Map (a, a) Int
+count2Gram xs = foldr (\gram m -> M.insertWith (+) gram 1 m)
+	M.empty $ grams xs
+	where
+		grams (x0:x1:xs) = (x0, x1) : grams (x1:xs)
+		grams _ = []
+
+-- | Generate rank-1 markov sequence from 2-gram frequency table.
+genMC1 :: RandomGen g => M.Map (String, String) Int -> Rand g [String]
+genMC1 freq2 = sampleAll "import"
+	where
+		sampleAll prev = do
+			m_next <- sampleNext prev
+			case m_next of
+				Nothing -> return [prev]
+				Just next -> return (prev :) `ap` (sampleAll next)
+
+		sampleNext :: RandomGen g => String -> Rand g (Maybe String)
+		sampleNext prev = case M.lookup prev shiftProbTable of
+			Nothing -> return Nothing
+			Just table -> return (Just . sampleRussian table) `ap`
+				(getRandomR (0.0, 1.0))
+
+		sampleRussian :: [(Double, a)] -> Double -> a
+		sampleRussian [(_, x)] _ = x
+		sampleRussian ((p, x):elems) z
+			|z < p = x
+			|otherwise = sampleRussian elems (z - p)
+
+		shiftProbTable :: M.Map String [(Double, String)]
+		shiftProbTable = foldr
+			(\((from, to), count) m -> M.insertWith (++) from
+				[(fromIntegral count / fromIntegral (targetCount M.! from), to)] m)
+			M.empty $ M.assocs freq2
+
+		targetCount :: M.Map String Int
+		targetCount = foldr
+			(\((from, to), count) m -> M.insertWith (+) from count m)
+			M.empty $ M.assocs freq2
+	
+
 main = do
-	putStrLn =<< evalRandIO randomCode
+	c <- readFile "python-xyx/ylex.py"
+	let syms = splitter "" c ++ ["_____EOF_____"]
+	let s_syms = S.fromList syms
+	{-
+	print (length syms)
+	print (S.size s_syms)
+	print $ count2Gram syms
+	-}
+	let freq2 = count2Gram syms
+
+	putStrLn . concat =<< evalRandIO (genMC1 freq2)
+
+--	mapM_ print $ splitter "" c
+
+	-- putStrLn =<< evalRandIO randomCode
